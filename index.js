28c20999ec1b73193e30054e0456c5d5f6155cb1
Evans Ibok <evansibok@gmail.com>
2020-01-14 22:02:18 +0100

    create get request for single post using id

--- index.js
+++ index.js
@@ -13,16 +13,50 @@ const port = 5000;
 // Plug added functionalities to app
 server.use(express.json());
 
+// GETS
 server.get('/api/posts', (req, res) => {
   find()
     .then(posts => {
       res.status(200).json(posts);
     })
     .catch(error => {
-      res.status(500).json({ error: error.message });
+      res.status(500).json({
+        error: "The posts information could not be retrieved.",
+        stack: error.stack,
+      });
     });
 })
 
+server.get('/api/posts/:id', async (req, res) => {
+  const { id } = req.params;
+
+  const idCheck = await findById(id);
+
+  // Is post with ID found? No - Return 404, Yes - Proceed to fetch post
+  if (!idCheck.length) {
+    res.status(404).json({ message: "The post with the specified ID does not exist." })
+  } else {
+    // Does fetch return post? Yes - Return 200, No - Return 500
+    findById(id)
+      .then(post => {
+        res.status(200).json(post);
+      })
+      .catch(error => {
+        res.status().json({
+          error: "The post information could not be retrieved.",
+          stack: error.stack
+        });
+      });
+  }
+
+})
+
+server.get('/api/posts/:id/comments', (req, res) => {
+  const { id } = req.params;
+  const comment = req.body;
+})
+
+// POSTS
 server.post('/api/posts', (req, res) => {
   const content = req.body;
 
@@ -42,25 +76,7 @@ server.post('/api/posts', (req, res) => {
         res.status(500).json({ error: "There was an error while saving the post to the database" });
       });
   }
-})
-
-server.get('/api/posts/:id', (req, res) => {
-  const { id } = req.params;
-})
-
-server.delete('/api/posts/:id', (req, res) => {
-  const { id } = req.params;
-})
-
-server.put('/api/posts/:id', (req, res) => {
-  const { id } = req.params;
-  const { replacement } = req.body;
-})
-
-server.get('/api/posts/:id/comments', (req, res) => {
-  const { id } = req.params;
-  const comment = req.body;
-})
+});
 
 server.post('/api/posts/:id/comments', async (req, res) => {
   const { id } = req.params;
@@ -69,12 +85,12 @@ server.post('/api/posts/:id/comments', async (req, res) => {
   const idCheck = await findById(id);
 
   // Is ID found? No - Return 404,
-  if (!idCheck.length){
+  if (!idCheck.length) {
     res.status(404).json({ message: "The post with the specified ID does not exist." });
   } else {
 
     // Is comment.text available? No - Return 400, Yes - Return 201
-    if (!text){
+    if (!text) {
       res.status(400).json({ errorMessage: "Please provide text for the comment." });
     } else {
       insertComment({ text, post_id: id })
@@ -91,6 +107,20 @@ server.post('/api/posts/:id/comments', async (req, res) => {
     }
   }
 })
+
+// DELETE
+server.delete('/api/posts/:id', (req, res) => {
+  const { id } = req.params;
+})
+
+// PUT
+server.put('/api/posts/:id', (req, res) => {
+  const { id } = req.params;
+  const { replacement } = req.body;
+})
+
+
+
 
 
 // Spin up server to listen for incoming changes
